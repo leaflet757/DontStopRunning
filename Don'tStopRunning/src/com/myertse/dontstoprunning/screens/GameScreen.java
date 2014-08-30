97e8844f0db7991924df841436c7944e65460403
Samson Tse <sammy_tse@msn.com>
2014-08-30 11:26:17 -0700

    Debug issues
    Fixed Game according to proper rules

--- Don'tStopRunning/src/com/myertse/dontstoprunning/screens/GameScreen.java
+++ Don'tStopRunning/src/com/myertse/dontstoprunning/screens/GameScreen.java
@@ -15,7 +15,18 @@ public class GameScreen extends Screen {
 	//Boolean watch variable for left button
 	boolean isLeftPressed = false;
 	boolean isRightPressed = false;
+	
+	//the Pixmap to draw
+	Pixmap assetToDraw = Assets.protaganistMid;
+	
+	//where the person is touching
+	int touchX = 0;
+	int touchY = 0;
+	
 	int x = 0;
+	
+	//the lane that the character is currently in
+	int lane = 1;
 	
 	//will be hardocded eventually, y value should not move
 	int y = 0;
@@ -36,6 +47,9 @@ public class GameScreen extends Screen {
 		Log.d("GameScreen", "updating...");
 		Graphics g = GAME.getGraphics();
 		g.clear(1);
+		
+		
+		
 		//issue picture needs to scale		
 		g.drawPixmap(Assets.stepLeft, 0, g.getHeight() - Assets.stepLeft.getHeight());
 		g.drawPixmap(Assets.stepRight, g.getWidth()/2, g.getHeight() - Assets.stepRight.getHeight());
@@ -44,14 +58,14 @@ public class GameScreen extends Screen {
 		if(GAME.getInput().isTouchDown(0))
 		{
 			
-			x = GAME.getInput().getTouchX(0);
-			y = GAME.getInput().getTouchY(0);
-			if(x < (g.getWidth()/2) && y > (g.getHeight() - g.getHeight()/4))
-			{
+			touchX = GAME.getInput().getTouchX(0);
+			touchY = GAME.getInput().getTouchY(0);
+			if(touchX < (g.getWidth()/2) && touchY > (g.getHeight() - g.getHeight()/4))
+			{	
 				location--;
 				isLeftPressed = true;
 			}
-			if(x > (g.getWidth()/2) && y > (g.getHeight() - g.getHeight()/4))
+			if(touchX > (g.getWidth()/2) && touchY > (g.getHeight() - g.getHeight()/4))
 			{
 				location++;
 				isRightPressed = true;
@@ -62,51 +76,78 @@ public class GameScreen extends Screen {
 			{
 				//jump condition
 			}
-			else if(isLeftPressed)
-			{
-				isRightPressed = false;
-			}
-			else
-			{
-				isLeftPressed = false;
-			}
-			//g.drawPixmap(Assets.protaganistMid, x, y);
-			
-		}
-		else
-		{
-			
-		}
-		
+		}		
 	}
 
 	@Override
 	public void present(float deltaTime) {
 		Graphics g = GAME.getGraphics();
 		// TODO Auto-generated method stub
-		Pixmap assetToDraw = Assets.protaganistMid;
-		if(location < 0)
+		
+
+		//Hard coded location values
+		
+				int xLeftLane = 0;
+				int xMidLane = g.getWidth()/3;
+				int xRightLane = g.getWidth() - g.getWidth()/3;
+				int[] lanes = {xLeftLane, xMidLane, xRightLane};
+		if(isLeftPressed)
 		{
-			
+			Assets.tap.play(1);
 			assetToDraw = Assets.protaganistLeft;
-			x = 0;
-			//force minimum cap limit
-			location = -1;
+			if(location < -1)
+			{
+				if(lane > 0)
+				{
+					lane--;
+				}
+				location = 0;
+			}
+			
+			isLeftPressed = false;
+			
+			//debug purposes
+			try
+			{
+				Thread.sleep(500);
+			}
+			catch(Exception e)
+			{
+			}
+			
 		}
-		else if(location > 0)
+		else if(isRightPressed)
 		{
+			Assets.explosion.play(1);
 			assetToDraw = Assets.protaganistRight;
-			x = g.getWidth() - g.getWidth()/3;
-			//force max cap limit
-			location = 1;
+			if(location > 1)
+			{
+				if(lane < 2)
+				{
+					lane++;
+				}
+				location = 0;
+			}
+			isRightPressed = false;
+			
+			//debug purposes
+			try
+			{
+				Thread.sleep(500);
+			}
+			catch(Exception e)
+			{
+			}
 		}
 		else
 		{
-			x = g.getWidth()/3;
+			//assetToDraw = Assets.protaganistMid;
 		}
+		x = lanes[lane];
+		
 		if(lastLocation != location)
 		{
-			Assets.tap.play(1);
+			//Assets.tap.play(1);
 		}
 		//hard code height
 		y = g.getHeight()/2;
